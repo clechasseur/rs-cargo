87b7027c1ceb0631e59cee817da2ca2472964a98
Charles Lechasseur <shiftingbeard@gmx.com>
2023-08-05 04:28:21 -0400

    Use new matchers, thanks to actions-rust-lang/setup-rust-toolchain

--- src/main.ts
+++ src/main.ts
@@ -25,8 +25,10 @@ export async function run(actionInput: input.Input): Promise<void> {
 }
 
 async function main(): Promise<void> {
+  // Note: the matchers used here were copied from here:
+  // https://github.com/actions-rust-lang/setup-rust-toolchain/blob/12a4c2d9dc308e1b990c2f2e472947348bb41a20/rust.json
   const matchersPath = path.join(__dirname, ".matchers");
-  issueCommand('add-matcher', {}, path.join(matchersPath, "rust.json"));
+  issueCommand('add-matcher', {}, path.join(matchersPath, "rs-cargo.json"));
 
   const actionInput = input.get();
 
