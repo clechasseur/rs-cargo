e25d967d5b4aa856e8693ac1a7688afa68722bda
Charles Lechasseur <shiftingbeard@gmx.com>
2023-08-05 04:31:54 -0400

    Fix lint

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function main(): Promise<void> {
   // Note: the matchers used here were copied from here:
   // https://github.com/actions-rust-lang/setup-rust-toolchain/blob/12a4c2d9dc308e1b990c2f2e472947348bb41a20/rust.json
   const matchersPath = path.join(__dirname, ".matchers");
-  issueCommand('add-matcher', {}, path.join(matchersPath, "rs-cargo.json"));
+  issueCommand("add-matcher", {}, path.join(matchersPath, "rs-cargo.json"));
 
   const actionInput = input.get();
 
