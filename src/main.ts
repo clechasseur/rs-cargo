f193b87c5b10c943ff64a77e0f596ab61c592397
Charles Lechasseur <shiftingbeard@outlook.com>
2025-01-08 01:51:58 -0500

    fix: proper support of `toolchain` with `cargo-hack`

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,39 @@ import * as exec from "@actions/exec";
 import { issueCommand } from "@actions/core/lib/command";
 
 import * as input from "./input";
-import { Cargo, Cross, CargoHack } from "@clechasseur/rs-actions-core";
+import {
+  Cargo,
+  CargoHack,
+  CargoHackOptions,
+  Cross,
+  CrossOptions,
+} from "@clechasseur/rs-actions-core";
 
 async function getProgram(actionInput: input.Input) {
   switch (actionInput.tool) {
-    case "cross":
-      return await Cross.getOrInstall(actionInput.cacheKey);
-    case "cargo-hack":
-      return await CargoHack.getOrInstall(actionInput.cacheKey);
+    case "cross": {
+      const options: CrossOptions = {
+        toolchain: actionInput.toolchain,
+        primaryKey: actionInput.cacheKey,
+      };
+      return await Cross.getOrInstall(options);
+    }
+    case "cargo-hack": {
+      const options: CargoHackOptions = {
+        toolchain: actionInput.toolchain,
+        primaryKey: actionInput.cacheKey,
+      };
+      return await CargoHack.getOrInstall(options);
+    }
     default:
-      return await Cargo.get();
+      return await Cargo.get(actionInput.toolchain);
   }
 }
 
 export async function run(actionInput: input.Input): Promise<void> {
   const program = await getProgram(actionInput);
 
-  let args: string[] = [];
-  if (actionInput.toolchain) {
-    args.push(`+${actionInput.toolchain}`);
-  }
-  args.push(actionInput.command);
-  args = args.concat(actionInput.args);
+  const args = [actionInput.command, ...actionInput.args];
 
   const options: exec.ExecOptions = {};
   if (actionInput.workingDirectory) {
