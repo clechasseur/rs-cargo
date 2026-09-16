d292f7b8ad975dce5e03f59267ce2d0a3a292c51
Charles Lechasseur <shiftingbeard@outlook.com>
2026-09-15 23:39:53 -0400

    feat: update `@clechasseur/rs-actions-core` to 10.0.0

--- dist/run.d.ts
+++ dist/run.d.ts
@@ -1,4 +1,4 @@
 import * as input from "./input.js";
-import { Cargo, CargoHack, Cross } from "@clechasseur/rs-actions-core";
-export declare function getProgram(actionInput: input.Input): Promise<Cross | CargoHack | Cargo>;
+import { Cargo, CargoLike } from "@clechasseur/rs-actions-core";
+export declare function getProgram(actionInput: input.Input): Promise<CargoLike | Cargo>;
 export declare function run(actionInput: input.Input): Promise<void>;
